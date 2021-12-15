5c44cbbd90aefb3dfc57795f38bf56c7f9c2e7d4
absaGH <absa3852@gmail.com>
2021-12-15 13:07:04 +0000

    modified get function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,10 +10,30 @@ class Cache:
     """class to store Redis client information"""
 
     def __init__(self):
+        """ iniitalize Redis DB """
         self.___redis = redis.Redis()
         self.___redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ create a random key & stores it with data """
         key: uuid.UUID = uuid.uuid1()
         self.___redis.set(str(key), data)
         return str(key)
+
+    def get(self, key: str, fn: Optional[Callable]
+            = None) -> Union[str, bytes, int, float]:
+        """convert the data back to the desired format"""
+        if (!self.___redis.exists(key)):
+            return 0
+        if fn:
+            data = self.___redis.get(key)
+            data = fn(data)
+        return data
+
+    def get_str(self, data: bytes) -> str:
+        """convert data to string"""
+        return str(data, 'UTF-8')
+
+    def get_int(self, data: bytes) -> int:
+        """convert data to init"""
+        return int.from_bytes(data, "big")
